3d0869ef485f98df7d6aeaf3f725b07461076860
EC2 Default User <ec2-user@ip-172-31-23-154.us-east-2.compute.internal>
2018-02-11 20:24:50 +0000

    Updated API

--- server/api/controllers/loans.controller.js
+++ server/api/controllers/loans.controller.js
@@ -1,3 +1,5 @@
+import Transaction from '../../models/transaction';
+
 /**
  * Get all Loans
  * @param req
@@ -5,14 +7,40 @@
  * @returns void
  */
 export function getLoans(req, res) {
-  return res.send([
-    {
-      name: 'Sample loan',
-      health: 88,
-    },
-    {
-      name: 'Another Loan',
-      health: 20,
-    },
-  ]);
+  const loans = [];
+  return Transaction.aggregate([
+      { $group:
+        {
+          _id: '$loan_id',
+          successful_repay: { $sum: { $cmp: ['REPAYMENT_DECLINED', '$type'] } },
+          total_repay: { $sum: 1 },
+        }
+      },
+      { $lookup:
+        {
+          from: 'loans',
+          localField: '_id',
+          foreignField: '_id',
+          as: 'loan',
+        }
+      },
+      { $project: 
+        {
+          name: { $arrayElemAt: ['$loan.name', 0] },
+          health:
+            { 
+              $subtract: [
+                { $multiply: [{ $divide: ['$successful_repay', '$total_repay'] }, 100] },
+                { $mod: [{
+                  $multiply: [{ $divide: ['$successful_repay', '$total_repay'] }, 100] }, 1]
+                },
+              ]
+            },
+        }
+      },
+      { $sort: { health: 1 } }])
+   .cursor({ batchSize: 1000 })
+   .exec()
+   .on('data', loan => loans.push(loan))
+   .on('end', () => res.send(loans));
 }
